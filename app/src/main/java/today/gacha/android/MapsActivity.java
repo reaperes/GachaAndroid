31caf600207fa701f4a44016e9e1d3d665419e6b
reaperes <emerald105@hanmail.net>
2015-06-05 00:00:27 +0900

    [Task] Remove gacha framework

--- app/src/main/java/today/gacha/android/MapsActivity.java
+++ app/src/main/java/today/gacha/android/MapsActivity.java
@@ -1,32 +1,105 @@
 package today.gacha.android;
 
+import android.app.Dialog;
 import android.location.Location;
 import android.os.Bundle;
-import today.gacha.android.service.LocationActivityService;
-import today.gacha.android.service.MapActivityService;
-import today.gacha.gachaframework.GachaFragmentActivity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MapsActivity extends GachaFragmentActivity implements LocationActivityService.LocationChangedListener {
-	private LocationActivityService locationActivityService;
-	private MapActivityService mapActivityService;
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
+	GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-	{
-		mapActivityService = MapActivityService.with(this).mapFragmentId(R.id.map).getService();
-		locationActivityService = LocationActivityService.with(this).getService();
+	private static final String TAG = MapsActivity.class.getSimpleName();
 
-		registerActivityService(mapActivityService);
-		registerActivityService(locationActivityService);
-	}
+	private GoogleApiClient mGoogleApiClient;
+	private Location mLastLocation;
 
 	@Override
-	protected void onCreating(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_maps);
 
-		locationActivityService.setLocationChangedListener(this);
+		buildGoogleApiClient();
 	}
 
 	@Override
-	public void onLocationChanged(final Location location) {
-		mapActivityService.animateCamera(location.getLatitude(), location.getLongitude(), 14f, 200);
+	protected void onResume() {
+		super.onResume();
+
+		mGoogleApiClient.connect();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		mGoogleApiClient.disconnect();
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+		Log.e(TAG, "onConnected");
+
+		int state = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (state == ConnectionResult.SUCCESS) {
+			Log.e(TAG, "PlayService is SUCCESS");
+		}
+
+		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+		if (mLastLocation != null) {
+			Log.e(TAG, "Get last location success");
+
+			double latitude = mLastLocation.getLatitude();
+			double longitude = mLastLocation.getLongitude();
+
+			Log.e(TAG, "lat : " + String.valueOf(latitude));
+			Log.e(TAG, "lng : " + String.valueOf(longitude));
+		} else {
+			Log.e(TAG, "Request location update");
+
+			LocationRequest mLocationRequest = LocationRequest.create()
+				.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+				.setInterval(10 * 1000)        // 10 seconds, in milliseconds
+				.setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+			LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+		}
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		Log.e(TAG, "onLocationChanged");
+
+		double latitude = location.getLatitude();
+		double longitude = location.getLongitude();
+
+		Log.e(TAG, "lat : " + String.valueOf(latitude));
+		Log.e(TAG, "lng : " + String.valueOf(longitude));
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+		Log.e(TAG, "onConnectionSuspended");
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		Log.e(TAG, "onConnectionFailed");
+	}
+
+	private synchronized void buildGoogleApiClient() {
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+			.addConnectionCallbacks(this)
+			.addOnConnectionFailedListener(this)
+			.addApi(LocationServices.API)
+			.build();
 	}
 }
