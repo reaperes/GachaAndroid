ddf569dada90d9b723760d0ad16ec011094c637a
reaperes <emerald105@hanmail.net>
2015-06-05 23:55:02 +0900

    [Task] Set default location on nhn next.

--- app/src/main/java/today/gacha/android/MapsActivity.java
+++ app/src/main/java/today/gacha/android/MapsActivity.java
@@ -1,31 +1,50 @@
 package today.gacha.android;
 
-import android.app.Dialog;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
+/**
+ * Activity step
+ *
+ * 1. Launch activity with default map position (NHN NEXT).
+ * 2. Get user's last location.
+ */
 public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
-	GoogleApiClient.OnConnectionFailedListener, LocationListener {
+	GoogleApiClient.OnConnectionFailedListener, LocationListener, GoogleMap.OnMapClickListener {
 
 	private static final String TAG = MapsActivity.class.getSimpleName();
 
+	// default position
+	private static final LatLng NHN_NEXT = new LatLng(37.40208147037274d, 127.10891090333462);
+	private static final float NHN_NEXT_ZOOM = 12f;
+
 	private GoogleApiClient mGoogleApiClient;
 	private Location mLastLocation;
+
+	private GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_maps);
+
+		setUpMapIfNeeded();
+		googleMap.setOnMapClickListener(this);
+
 
 		buildGoogleApiClient();
 	}
@@ -60,8 +79,11 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
 			double latitude = mLastLocation.getLatitude();
 			double longitude = mLastLocation.getLongitude();
 
-			Log.e(TAG, "lat : " + String.valueOf(latitude));
-			Log.e(TAG, "lng : " + String.valueOf(longitude));
+			CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(latitude, longitude))
+				.zoom(14.5f).build();
+
+			Log.e(TAG, "Move camera to lat : " + String.valueOf(latitude) + ", lng : " + String.valueOf(longitude));
+				googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 		} else {
 			Log.e(TAG, "Request location update");
 
@@ -95,11 +117,23 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
 		Log.e(TAG, "onConnectionFailed");
 	}
 
+	@Override
+	public void onMapClick(LatLng latLng) {
+		Log.e(TAG, "Lat : " + String.valueOf(latLng.latitude) + ", Lng : " + String.valueOf(latLng.longitude));
+	}
+
 	private synchronized void buildGoogleApiClient() {
 		mGoogleApiClient = new GoogleApiClient.Builder(this)
 			.addConnectionCallbacks(this)
 			.addOnConnectionFailedListener(this)
 			.addApi(LocationServices.API)
 			.build();
+	}
+
+	private void setUpMapIfNeeded() {
+		if (googleMap == null) {
+			googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NHN_NEXT, NHN_NEXT_ZOOM));
+		}
 	}
 }
